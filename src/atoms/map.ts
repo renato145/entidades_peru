5d23f86732be96ea5fb7860c40f98f6b14eec601
Renato <renato145@hotmail.com>
2021-06-28 16:49:19 +0930

    add comments

--- src/atoms/map.ts
+++ src/atoms/map.ts
@@ -1,8 +1,10 @@
 import { atom } from "jotai";
 import { dataSummaryAtom } from "./data";
 
+/** Name of selected departamento */
 export const mapSelectionAtom = atom<string | null>(null);
 
+/** Creates atom to check status of an specific departamento */
 export const createSelectionStatusAtom = (check: string) =>
   atom((get) => {
     const mapSelection = get(mapSelectionAtom);
@@ -13,6 +15,7 @@ export const createSelectionStatusAtom = (check: string) =>
       : "other";
   });
 
+/** Data for the current selected departamento */
 export const selectedDataAtom = atom((get) => {
   const selection = get(mapSelectionAtom);
   if (selection === null) return null;
